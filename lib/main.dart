1e7366ad743b7fe967d82d6346b22935b2b9167b
yalda mohasseli <yaldamohasseliym@gmail.com>
2023-12-12 18:41:50 +0330

    Finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:seven_learn_excersize_two/strings.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,36 +12,330 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: '7Learn Exercise 2',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        useMaterial3: true,
-      ),
+          useMaterial3: true,
+          colorScheme: const ColorScheme.light(primary: Color(0xff176FF2))),
       home: const MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Container(
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            Color(0xffE7E9F3),
+            Color(0xffFFFFFF),
+          ],
+        )),
+        child: Scaffold(
+          body: ListView(
+            physics: const BouncingScrollPhysics(),
+            padding: const EdgeInsets.all(20),
+            children: const [
+              _Header(),
+              SizedBox(height: 16),
+              _Title(),
+              SizedBox(height: 16),
+              _Description(),
+              SizedBox(height: 32),
+              _Facilities(),
+              SizedBox(height: 30),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  _Price(),
+                  SizedBox(width: 56),
+                  Expanded(child: _BookButton()),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _Header extends StatelessWidget {
+  const _Header();
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+    return Stack(
+      clipBehavior: Clip.none,
+      children: [
+        ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: Image.asset('asset/image/image.png')),
+        const Positioned(left: 12, top: 12, child: _BackButton()),
+        const Positioned(right: 14, bottom: -20, child: _LikeButton())
+      ],
+    );
+  }
+}
+
+class _BackButton extends StatelessWidget {
+  const _BackButton();
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+        padding: const EdgeInsets.all(8),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8),
+            color: const Color(0xffF3F8FE)),
+        child: const Icon(
+          Icons.keyboard_arrow_left_rounded,
+          color: Color(0xffB8B8B8),
+        ),
+      ),
+    );
+  }
+}
+
+class _LikeButton extends StatelessWidget {
+  const _LikeButton();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: const Color(0xffF2F7FD),
+          boxShadow: [
+            BoxShadow(
+                color: const Color(0xff0038FF).withOpacity(.1),
+                offset: const Offset(0, 6),
+                blurRadius: 8)
+          ]),
+      child: SvgPicture.asset('asset/image/Heart.svg'),
+    );
+  }
+}
+
+class _Title extends StatelessWidget {
+  const _Title();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Text(
+              'Coeurdes Alpes',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+            ),
+            TextButton(
+              onPressed: () {},
+              child: Text('Show map',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 14,
+                      color: Theme.of(context).colorScheme.primary)),
             ),
           ],
         ),
+        Row(
+          children: [
+            SvgPicture.asset('asset/image/star.svg'),
+            const SizedBox(width: 4),
+            const Text('4.5 (355 Reviews)',
+                style: TextStyle(fontSize: 12, color: Color(0xff606060)))
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class _Description extends StatefulWidget {
+  const _Description();
+
+  @override
+  State<_Description> createState() => _DescriptionState();
+}
+
+class _DescriptionState extends State<_Description> {
+  bool _readMore = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          _readMore ? textSummery : AppStrings.description,
+          textAlign: TextAlign.justify,
+          style: const TextStyle(color: Color(0xff3A544F), fontSize: 14),
+        ),
+        _ReadMoreButton(
+          readMore: _readMore,
+          onTap: () => setState(() => _readMore = !_readMore),
+        )
+      ],
+    );
+  }
+
+  String get textSummery =>
+      '${AppStrings.description.substring(0, AppStrings.description.length ~/ 3)} ....';
+}
+
+class _ReadMoreButton extends StatelessWidget {
+  const _ReadMoreButton({required this.readMore, required this.onTap});
+
+  final bool readMore;
+  final Function() onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: TextButton.icon(
+        onPressed: onTap,
+        icon: Icon(readMore
+            ? Icons.keyboard_arrow_down_rounded
+            : Icons.keyboard_arrow_up_rounded),
+        label: Text(readMore ? 'Read more' : 'Read less'),
+        style: TextButton.styleFrom(padding: EdgeInsets.zero),
       ),
+    );
+  }
+}
+
+class _Facilities extends StatelessWidget {
+  const _Facilities();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text(
+          'Facilities',
+          style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+        ),
+        const SizedBox(height: 16),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            _FacilityItem(
+                icon: SvgPicture.asset('asset/image/heater.svg'),
+                title: '1 Heater'),
+            _FacilityItem(
+                icon: SvgPicture.asset('asset/image/food.svg'),
+                title: 'Dinner'),
+            _FacilityItem(
+                icon: SvgPicture.asset('asset/image/bath.svg'), title: '1 Tub'),
+            _FacilityItem(
+                icon: SvgPicture.asset('asset/image/pool.svg'), title: 'Pool'),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class _FacilityItem extends StatelessWidget {
+  const _FacilityItem({required this.icon, required this.title});
+
+  final String title;
+  final Widget icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 80,
+      padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 20),
+      decoration: BoxDecoration(
+          color: const Color(0xffEFF4FC),
+          borderRadius: BorderRadius.circular(16)),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          icon,
+          const SizedBox(height: 6),
+          Text(
+            title,
+            style: const TextStyle(color: Color(0xffB8B8B8), fontSize: 10),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class _Price extends StatelessWidget {
+  const _Price();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Price',
+          style: TextStyle(fontSize: 12, fontWeight: FontWeight.w500),
+        ),
+        Text(
+          '\$199',
+          style: TextStyle(
+              color: Color(0xff2DD7A4),
+              fontSize: 24,
+              fontWeight: FontWeight.bold),
+        ),
+      ],
+    );
+  }
+}
+
+class _BookButton extends StatelessWidget {
+  const _BookButton();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          gradient: LinearGradient(
+            begin: Alignment.centerLeft,
+            end: Alignment.centerRight,
+            colors: [
+              Theme.of(context).colorScheme.primary,
+              const Color(0xff196EEE),
+            ],
+          ),
+          boxShadow: [
+            BoxShadow(
+                color: const Color(0xff0038FF).withOpacity(0.24),
+                blurRadius: 15,
+                offset: const Offset(0, 6))
+          ]),
+      child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+        const Text(
+          'Book Now!',
+          style: TextStyle(
+              color: Colors.white, fontSize: 16, fontWeight: FontWeight.w500),
+        ),
+        const SizedBox(width: 10),
+        SvgPicture.asset('asset/image/Arrow-Right.svg')
+      ]),
     );
   }
 }
---
+++ lib/strings.dart
@@ -0,0 +1,6 @@
+class AppStrings {
+  AppStrings._();
+
+  static const String description =
+      "Aspen is as close as one can get to a storybook alpine town in America, offering a myriad of choose-your-own-adventure possibilities. From skiing down world-class slopes in the winter to hiking through lush landscapes and discovering hidden alpine lakes in the warmer months, Aspen is a year-round playground for outdoor enthusiasts. The charming streets, lined with Victorian-style buildings and boutique shops, invite visitors to explore, while the surrounding majestic mountains provide a breathtaking backdrop. In winter, Aspen transforms into a snowy wonderland, attracting global skiers and snowboarders who can later unwind in the après-ski atmosphere of cozy fireside gatherings and upscale dining experiences. The vibrant arts and culture scene adds an extra layer of allure, allowing visitors to immerse themselves in the town's rich cultural tapestry. Culinary enthusiasts can indulge in a diverse array of dining options, ranging from upscale restaurants to quaint cafes, showcasing the region's fresh and flavorful cuisine. The fusion of nature, adventure, and cultural richness makes Aspen an idyllic destination, ensuring that every visit unfolds like a chapter in a captivating storybook.";
+}
